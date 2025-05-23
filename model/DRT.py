f20ff11f4d3b84e151d3b9fd3ca5bf7b029add10
aaron.hx <imhuim982@126.com>
2025-05-23 17:55:23 +0800

    update GCA

--- model/DRT.py
+++ model/DRT.py
@@ -1,22 +1,6 @@
 # coding=utf-8
-# Copyright 2022 EleutherAI and the HuggingFace Inc. team. All rights reserved.
-#
-# This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
-# and OPT implementations in this library. It has been modified from its
-# original forms to accommodate minor architectural differences compared
-# to GPT-NeoX and OPT used by the Meta AI team that trained the model.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2025 Ant Group
+
 """ PyTorch LLaMA model. Based on transformers==4.35.2"""
 import math
 import warnings
@@ -27,7 +11,6 @@ import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
 from torch import nn
-# from torch.nn import BCEWithLogitsLoss, MSELoss
 from einops import rearrange, repeat
 from transformers.generation import GenerationMixin
 
